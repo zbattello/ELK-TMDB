053dde14b8179292aed00e2030ab049fcdba36ee
adamSbaghi <115209958+adamSbaghi@users.noreply.github.com>
2024-01-12 10:39:06 +0100

    script test elasticsearch

---
+++ es.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from elasticsearch import Elasticsearch
+
+ELASTIC_PASSWORD = "=0aZNCDEppl_i*QOlWly"
+
+#disable certificate
+es = Elasticsearch(hosts="https://localhost:9200", basic_auth=("elastic", ELASTIC_PASSWORD), verify_certs=False)
+
+
+
+
+
+doc = {
+    'author': 'kimchy',
+    'text': 'Elasticsearch: cool. bonsai cool.',
+    'timestamp': datetime.now(),
+}
+resp = es.index(index="test-index", id=1, document=doc)
+print(resp['result'])
+
+resp = es.get(index="test-index", id=1)
+print(resp['_source'])
+
+es.indices.refresh(index="test-index")
+
+resp = es.search(index="test-index", query={"match_all": {}})
+print("Got %d Hits:" % resp['hits']['total']['value'])
+for hit in resp['hits']['hits']:
+    print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
