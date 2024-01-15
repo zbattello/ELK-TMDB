619232d180d507f2cefa268243546dd8565694e2
zbattello <zoe.battello@insa-lyon.fr>
2024-01-15 09:54:24 +0100

    add list medias

--- allmedias.py
+++ allmedias.py
@@ -1,8 +1,10 @@
+import datetime
 import requests
 import csv
 import time
+import calendar
 
-# API key and URL TMBD
+# API key and URL TMDB
 api_key = '1d2a958442de161a2791dc703cb9dcbc'
 base_url = 'https://api.themoviedb.org/3/'
 
@@ -20,37 +22,74 @@ def get_existing_ids(csv_filename):
     return existing_ids
 
 # Function to write or append data to CSV
-def write_or_append_to_csv(csv_filename, url, is_movie, existing_ids, start_page, end_page):
+def write_or_append_to_csv(csv_filename, data, is_movie, existing_ids):
     with open(csv_filename, 'a', newline='', encoding='utf-8') as file:
         csv_writer = csv.writer(file)
-        # Write headers only if the file is newly created
-        if not existing_ids and start_page == 1:
+        if not existing_ids:
             headers = ['ID', 'Title', 'Vote Average', 'Date', 'Original Language', 'Genre IDs', 'IsMovie']
             csv_writer.writerow(headers)
 
-        for page in range(start_page, end_page + 1):
-            if page > end_page:  # To avoid going over the API limit
-                        break
-            response = requests.get(f'{url}&page={page}')
-            if response.status_code != 200:
-                print(f"Error fetching page {page}: {response.status_code} - {response.text}")
-                break
+        for item in data:
+            id = str(item.get('id'))  # Ensure ID is a string for set comparison
+            if id not in existing_ids:
+                title = item.get('title') if is_movie else item.get('name')
+                vote_average = item.get('vote_average')
+                date = item.get('release_date') if is_movie else item.get('first_air_date')
+                original_language = item.get('original_language')
+                genre_ids = item.get('genre_ids')
+                csv_writer.writerow([id, title, vote_average, date, original_language, genre_ids, is_movie])
+                existing_ids.add(id)
 
-            data = response.json()
+def process_media(url, is_movie, csv_filename, existing_ids):
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Error fetching data: {response.status_code} - {response.text}")
+        return 0
 
-            if 'results' in data:
-                for item in data['results']:
-                    id = str(item.get('id'))  # Ensure ID is a string for set comparison
-                    if id not in existing_ids:
-                        title = item.get('title') if is_movie else item.get('name')
-                        vote_average = item.get('vote_average')
-                        date = item.get('release_date') if is_movie else item.get('first_air_date')
-                        original_language = item.get('original_language')
-                        genre_ids = item.get('genre_ids')
-                        csv_writer.writerow([id, title, vote_average, date, original_language, genre_ids, is_movie])
-                        existing_ids.add(id)
+    data = response.json()
+    total_pages = min(data['total_pages'], 500)  # Limit to 500 pages
+    total_results = 0  # Initialize total results count
 
-            time.sleep(1)  # Delay to avoid hitting rate limits
+    for page in range(1, total_pages + 1):
+        response = requests.get(f'{url}&page={page}')
+        if response.status_code != 200:
+            print(f"Error fetching page {page}: {response.status_code} - {response.text}")
+            continue
+
+        page_data = response.json().get('results', [])
+        write_or_append_to_csv(csv_filename, page_data, is_movie, existing_ids)
+        total_results += len(page_data)
+        time.sleep(5)  # Delay to avoid hitting rate limits
+
+    return total_results  # Return the count of processed items
+
+# Function to iterate over months and process media
+def iterate_and_process_dates(start_date, end_date, output_filename):
+    current_date = start_date
+    while current_date <= end_date:
+        # Get the last day of the current month
+        last_day_of_month = calendar.monthrange(current_date.year, current_date.month)[1]
+        start_date_str = current_date.strftime('%Y-%m-%d')
+        end_date_str = current_date.replace(day=last_day_of_month).strftime('%Y-%m-%d')
+
+        url_tv_shows = f'{base_url}discover/tv?api_key={api_key}&first_air_date.gte={start_date_str}&first_air_date.lte={end_date_str}'
+        url_movies = f'{base_url}discover/movie?api_key={api_key}&primary_release_date.gte={start_date_str}&primary_release_date.lte={end_date_str}'
+
+        existing_ids = get_existing_ids(output_filename)
+
+        tv_show_count = process_media(url_tv_shows, False, output_filename, existing_ids)
+        movie_count = process_media(url_movies, True, output_filename, existing_ids)
+
+        if movie_count != 0 or tv_show_count != 0:
+            print(f"Month: {current_date.strftime('%Y-%m')}, Movies: {movie_count}, TV Shows: {tv_show_count}")
+
+        # Increment the date to the first day of the next month
+        if current_date.month == 12:
+            current_date = datetime.date(current_date.year + 1, 1, 1)
+        else:
+            current_date = datetime.date(current_date.year, current_date.month + 1, 1)
+        time.sleep(1)  # Delay to avoid hitting rate limits
+
 
 # Output filename
 output_filename = 'media_list.csv'
@@ -58,28 +97,13 @@ output_filename = 'media_list.csv'
 # Get existing IDs from the CSV
 existing_ids = get_existing_ids(output_filename)
 
-# Function to process movies or TV shows in chunks
-def process_in_chunks(url, is_movie, total_pages, chunk_size=500):
-    for start_page in range(1, total_pages, chunk_size):
-        start_time = time.time()  # Start time
+# Define the start and end dates for iteration
+#start_date = datetime.date(1874, 12, 8) # first date
+start_date = datetime.date(2023, 3, 1)
+end_date = datetime.date.today()  # or any end date you prefer
 
-        end_page = min(start_page + chunk_size - 1, total_pages)
-        print('isMovie: ' + str(is_movie) + ' - retrieve from page: ' + str(start_page) + ' to ' + str(end_page))
+# Output filename
+output_filename = 'media_list.csv'
 
-        write_or_append_to_csv(output_filename, url, is_movie, existing_ids, start_page, end_page)
-
-        end_time = time.time()  # End time
-        elapsed_time = end_time - start_time  
-        print('end chunk - Time taken: {:.2f} seconds'.format(elapsed_time))
-
-# Define total pages for movies and TV shows
-total_movie_pages = 41848
-total_tv_show_pages = 8107
-
-# URLs for movies and TV shows
-url_movies = f'{base_url}discover/movie?api_key={api_key}&year=2024'
-url_tv_shows = f'{base_url}discover/tv?api_key={api_key}&year=2024'
-
-# Process movies and TV shows in chunks
-process_in_chunks(url_movies, True, total_movie_pages)
-process_in_chunks(url_tv_shows, False, total_tv_show_pages)
+# Start the iteration process
+iterate_and_process_dates(start_date, end_date, output_filename)
