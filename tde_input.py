dec3b832e711aa48bd9b1123ebb809224159d3e3
zbattello <zoe.battello@insa-lyon.fr>
2024-01-19 11:34:41 +0100

    update script tde envoi

--- tde_input.py
+++ tde_input.py
@@ -2,28 +2,26 @@ import pandas as pd
 import http.client
 import json
 
-def send_data_to_elasticsearch(csv_file_path, es_host, es_port, index_name):
-    # Connexion au serveur Elasticsearch
-    connection = http.client.HTTPConnection(es_host, es_port)
+def send_data_to_TDE(csv_file_path, host, port, index_name):
+    # HTTP request to Elasticsearch
+    connection = http.client.HTTPConnection(host, port)
 
-    # Lecture du fichier CSV avec Pandas
+    # fichier CSV 
     df = pd.read_csv(csv_file_path)
 
     
-    # Envoi des données à Elasticsearch
+    # envoi à elastic search tdE
     for _, row in df.iterrows():
         json_data = json.dumps(row.to_dict())
         connection.request("POST", f"/{index_name}/tmdbdoc/", json_data, headers={"Content-Type": "application/json"})
         response = connection.getresponse()
-        # print row number
         print(f"{row.name} : {response.read().decode()}")
 
-    # Fermeture de la connexion
     connection.close()
 
 if __name__ == '__main__':
     csv_file_path = './media_list.csv'
-    es_host = "134.214.202.38"
-    es_port = 9200
+    host = "134.214.202.38"
+    port = 9200
     index_name = "tmdb"
-    send_data_to_elasticsearch(csv_file_path, es_host, es_port, index_name)
+    send_data_to_TDE(csv_file_path, host, port, index_name)
